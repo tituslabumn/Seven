200f1ea0c236583a7521fd0ef8fafb5a3ac337fc
presbyope <42038041+presbyope@users.noreply.github.com>
2019-03-28 13:31:36 +0100

    Revert "feature: additional global arrays"
    This reverts commit 6dc871f1c4b20fbccb7c6c196bfb7e1f3ae47b31.

--- seven_multi.js
+++ seven_multi.js
@@ -6,16 +6,9 @@ load(IJ.getDirectory("ImageJ")+"seven.js");
 var dc = new DirectoryChooser("Choose the experiment analysis directory:");
 var root = new File(dc.getDirectory());
 
-	// fp data - probably not necessary as global (only referenced in AnalyzeTips())
 	var cell_per_fp = new Array(); // global array for registering filopodia to cells
 	var intensity_per_fp = new Array(); // global array for filopod tip intensity
-	var xpos_per_fp = new Array(); // global array for filopod raw X coordinate (in pixels)
-	var ypos_per_fp = new Array(); // global array for filopod raw Y coordinate (in pixels)
-
-	// cell data - assigned in AnalyzeCells() and referenced again in AnalyzeTips()
 	var cell_body = new Array(); // global array for cell body intensity
-	var cell_xpos = new Array(); // global array for cell raw X coordinate (in pixels)
-	var cell_ypos = new Array(); // global array for cell raw Y coordinate (in pixels)
 	var cell_tipavg = new Array(); // global array for cell average tip intensity
 	var cell_band = new Array(); // global array for cell band intensity
 	var cell_area_body = new Array(); // global array for area of cell body
